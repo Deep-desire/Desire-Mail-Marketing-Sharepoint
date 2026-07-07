f423ee66c9d714430d0c9c8cb01b980c1af36b88
Deep-desire <deep_desire132@outlook.com>
2026-07-07 11:09:03 +0530

    add upadte

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:7071',
+        target: 'http://127.0.0.1:7071',
         changeOrigin: true,
       },
     },
